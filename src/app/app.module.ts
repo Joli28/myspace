9142fa231554d9f2eee4dd904231ed130c56e56d
Joli28 <you@example.com>
2021-05-20 15:56:59 +0200

    myspace

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FeaturedComponent } from './featured/featured.component';
+import { MusicComponent } from './music/music.component';
+import { VideosComponent } from './videos/videos.component';
+import { ProfilesComponent } from './profiles/profiles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeaturedComponent,
+    MusicComponent,
+    VideosComponent,
+    ProfilesComponent
   ],
   imports: [
     BrowserModule
